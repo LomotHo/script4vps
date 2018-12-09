43ab3537ed0a101a3d821e765bcf67f00fce44f5
lomot <lomot@qq.com>
2018-12-09 23:19:08 +0800

    test ok

--- openvz.sh
+++ openvz.sh
@@ -1,5 +1,10 @@
 #!/bin/sh	
 
+USER_HOME=/root
+ZSH=$USER_HOME/.oh-my-zsh
+cd $USER_HOME
+
+# add swap
 echo 'Start adding SWAP space ......';
 SWAP="${1:-512}";
 NEW="$[SWAP*1024]";
@@ -10,26 +15,38 @@ sed "/^Swap\(Total\|Free\):/s,$OLD,$NEW," /proc/meminfo > /etc/fake_meminfo
 mount --bind /etc/fake_meminfo /proc/meminfo
 echo 'Add SWAP ready!';
 
+# install software
 apt update
 apt install htop tmux zsh git curl -y
 
-# ========== add user now ==========
-
-wget https://github.com/tcp-nanqinlang/lkl-rinetd/releases/download/1.1.0/tcp_nanqinlang-rinetd-centos.sh
-chmod +x tcp_nanqinlang-rinetd-debianorubuntu.sh
-
+# download script
+wget --no-check-certificate -O rinetd-bbr.sh https://github.com/tcp-nanqinlang/lkl-rinetd/releases/download/1.1.0/tcp_nanqinlang-rinetd-centos.sh
 wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
-chmod +x shadowsocks-all.sh
+chmod +x $USER_HOME/*.sh
 
-# # add ssh key
-mkdir ~/.ssh
-chmod 700 ~/.ssh
-cp ./config/authorized_keys ~/.ssh
-chmod 600 ~/.ssh/authorized_keys
+# install tmux, zsh config
+git clone https://github.com/LomotHo/lomot_dotfile.git $USER_HOME/.lomot_dotfile
+ln -s $USER_HOME/.lomot_dotfile/zsh/ohmyzsh/server.zshrc $USER_HOME/.zshrc
+ln -s $USER_HOME/.lomot_dotfile/.tmux.conf $USER_HOME/.tmux.conf
 
-# *** install tmux, zsh config
-git clone https://github.com/LomotHo/lomot_dotfile.git ~/.lomot_dotfile
-ln -s ~/.lomot_dotfile/zsh/ohmyzsh/server.zshrc ~/.zshrc
-ln -s ~/.lomot_dotfile/.tmux.conf ~/.tmux.conf
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# add ssh key
+# 检查是否存在 $USER_HOME/.ssh
+if [ ! -d "$USER_HOME/.ssh" ]; then
+  echo "no $USER_HOME/.ssh, mkdir $USER_HOME/.ssh, chmod it to 700"
+  mkdir $USER_HOME/.ssh
+  chmod 700 $USER_HOME/.ssh
+else 
+  echo "$USER_HOME/.ssh existed, chmod it to 700"
+  chmod 700 $USER_HOME/.ssh
+fi
+# 复制$USER_HOME/.ssh/authorized_keys
+echo "******copy new authorized_keys******"
+git clone https://github.com/LomotHo/script4vps.git
+cp $USER_HOME/script4vps/config/authorized_keys $USER_HOME/.ssh
+chmod 600 $USER_HOME/.ssh/authorized_keys
 
+# install zsh
+git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git "$ZSH"
+echo "run bbr.sh & shadowsocks-all.sh"
+chsh -s $(which zsh)
+zsh
