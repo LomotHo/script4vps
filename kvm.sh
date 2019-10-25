0f2f69e9e4004de41f8b852d9624b0ac60c0414c
lomot <lomot@qq.com>
2019-10-25 18:02:37 +0800

    add v2ray bbr

--- kvm.sh
+++ kvm.sh
@@ -24,12 +24,12 @@ apt install htop vim tmux zsh git curl -y
 
 
 # download script
-wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
+wget -N --no-check-certificate -O shadowsocks-all.sh "https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh"
+wget -N --no-check-certificate -O linux-netspeed.sh "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh"
+wget -N --no-check-certificate -O v2ray.sh "https://install.direct/go.sh"
 chmod +x $USER_HOME/*.sh
-git clone https://github.com/cx9208/Linux-NetSpeed.git $USER_HOME
 git clone https://github.com/LomotHo/script4vps.git $SCRIPT4VPS_HOME
 # cp -r $USER_HOME/script4vps/dotfile $DOT_FILE_HOME
-
 
 # add ssh key
 # check if $USER_HOME/.ssh exist
