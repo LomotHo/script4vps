d6d1f8801cf86c6a7ff525d6731f3ebed1b3853d
lomot <lomot@qq.com>
2019-11-02 10:51:38 +0800

    add vps.vimrc

---
+++ update-config.sh
@@ -0,0 +1,6 @@
+rm ~/.tmux.conf
+rm ~/.zshrc
+rm ~/.vimrc
+ln -s ~/.script4vps/config/vps.tmux.conf ~/.tmux.conf
+ln -s ~/.script4vps/config/vps.zshrc ~/.zshrc
+ln -s ~/.script4vps/config/vps.vimrc ~/.vimrc
