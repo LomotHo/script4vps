23829e06934d3b83cd56dd1da07e5e75ac39e90e
lomot <lomot@qq.com>
2019-11-29 15:35:28 +0800

    fix bug

--- update-config.sh
+++ update-config.sh
@@ -4,6 +4,7 @@ git pull
 rm ~/.tmux.conf
 rm ~/.zshrc
 rm ~/.vimrc
+rm ~/.gitconfig
 ln -s ~/.script4vps/config/vps.tmux.conf ~/.tmux.conf
 ln -s ~/.script4vps/config/vps.zshrc ~/.zshrc
 ln -s ~/.script4vps/config/vps.vimrc ~/.vimrc
