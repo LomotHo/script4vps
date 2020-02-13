aab510f906e0dcc53bae0280b830933903edbada
lomot <lomot@qq.com>
2020-02-13 19:27:25 +0800

    fix bug

--- src/download-script.sh
+++ src/download-script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # download script
-wget -N -nv --no-check-certificate -P $USER_HOME/ -O shadowsocks-all.sh "https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh"
-wget -N -nv --no-check-certificate -P $USER_HOME/ -O linux-netspeed.sh "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh"
-wget -N -nv --no-check-certificate -P $USER_HOME/ -O v2ray.sh "https://install.direct/go.sh"
+wget -nv --no-check-certificate -P $USER_HOME/ -O shadowsocks-all.sh "https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh"
+wget -nv --no-check-certificate -P $USER_HOME/ -O linux-netspeed.sh "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh"
+wget -nv --no-check-certificate -P $USER_HOME/ -O v2ray.sh "https://install.direct/go.sh"
 chmod +x $USER_HOME/*.sh
 git clone https://github.com/LomotHo/script4vps.git $SCRIPT4VPS_HOME
 # cp -r $USER_HOME/script4vps/dotfile $DOT_FILE_HOME
