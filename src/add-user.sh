f3aad1b0ae454576125748ef68c4566d5ded26b5
lomot <lomot@qq.com>
2020-02-13 22:50:13 +0800

    independent script

--- src/add-user.sh
+++ src/add-user.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+USER_NAME=${USER_NAME:-lomot}
+USER_HOME=${USER_HOME:-/home/$USER_NAME}
+
 # add user
 adduser $USER_NAME
 usermod -aG sudo $USER_NAME
--- src/install-software.sh
+++ src/install-software.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 # install software
 apt update
