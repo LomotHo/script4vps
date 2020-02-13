0ab133fb63f44024ef1e6a792a011e1fc42a0eb7
lomot <lomot@qq.com>
2020-02-13 23:33:49 +0800

    add config

--- src/install-script4vps.sh
+++ src/install-script4vps.sh
@@ -15,3 +15,7 @@ ln -s $VPS_CONFIG_HOME/vps.zshrc $USER_HOME/.zshrc
 ln -s $VPS_CONFIG_HOME/vps.tmux.conf $USER_HOME/.tmux.conf
 ln -s $VPS_CONFIG_HOME/vps.vimrc $USER_HOME/.vimrc
 ln -s $VPS_CONFIG_HOME/vps.gitconfig $USER_HOME/.gitconfig
+
+echo "DefaultTimeoutStartSec=3s" >> /etc/systemd/system.conf
+echo "DefaultTimeoutStopSec=3s" >> /etc/systemd/system.conf
+systemctl daemon-reload
