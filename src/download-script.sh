e9cd242d1603cfd0451fc969110eebabb895d803
lomot <lomot@qq.com>
2020-02-13 21:43:10 +0800

    fix bug

--- src/download-script.sh
+++ src/download-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # download script
-wget -nv --no-check-certificate -P $USER_HOME -O shadowsocks-all.sh "https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh"
-wget -nv --no-check-certificate -P $USER_HOME -O linux-netspeed.sh "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh"
-wget -nv --no-check-certificate -P $USER_HOME -O v2ray.sh "https://install.direct/go.sh"
+wget -nv --no-check-certificate -O $USER_HOME/shadowsocks-all.sh "https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh"
+wget -nv --no-check-certificate -O $USER_HOME/linux-netspeed.sh "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh"
+wget -nv --no-check-certificate -O $USER_HOME/v2ray.sh "https://install.direct/go.sh"
 chmod +x $USER_HOME/*.sh
