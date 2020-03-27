b68600cb54c6ae39e78c573d463896e9486ff9d8
lomot <lomot@qq.com>
2020-03-27 19:51:28 +0800

    quick commit

--- update-config.sh
+++ update-config.sh
@@ -5,8 +5,9 @@ rm ~/.tmux.conf
 rm ~/.zshrc
 rm ~/.vimrc
 rm ~/.gitconfig
+ln -s ~/.script4vps/config/vps.p10k.zsh ~/.p10k.zsh
 ln -s ~/.script4vps/config/vps.tmux.conf ~/.tmux.conf
-ln -s ~/.script4vps/config/vps.omz.zshrc ~/.zshrc
+ln -s ~/.script4vps/config/vps.zinit.zshrc ~/.zshrc
 ln -s ~/.script4vps/config/vps.vimrc ~/.vimrc
 ln -s ~/.script4vps/config/vps.gitconfig ~/.gitconfig
 echo "[ok] update config"
