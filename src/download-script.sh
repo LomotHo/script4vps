af919718cd60dca0518dfc57e365b21ddf0ebd18
lomot <lomot@qq.com>
2020-03-27 20:54:17 +0800

    rm ss.sh

--- src/download-script.sh
+++ src/download-script.sh
@@ -5,7 +5,7 @@ USER_NAME=${USER_NAME:-lomot}
 USER_HOME=${USER_HOME:-/home/$USER_NAME}
 
 # download script
-wget -nv --no-check-certificate -O $USER_HOME/shadowsocks-all.sh "https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh"
+# wget -nv --no-check-certificate -O $USER_HOME/shadowsocks-all.sh "https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh"
 wget -nv --no-check-certificate -O $USER_HOME/linux-netspeed.sh "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh"
 wget -nv --no-check-certificate -O $USER_HOME/v2ray.sh "https://install.direct/go.sh"
 chmod +x $USER_HOME/*.sh
