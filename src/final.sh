49d90b6d4a2e1e6f52b75170ca0e36e5bbd218a2
lomot <lomot@qq.com>
2020-12-24 14:35:13 +0800

    fix bug

--- src/final.sh
+++ src/final.sh
@@ -8,3 +8,6 @@ chown -R $USER_NAME:$USER_NAME $USER_HOME/.*
 
 log-info "chsh -s $(which zsh)"
 sudo -u ${USER_NAME} chsh -s $(which zsh)
+
+log-info "newgrp docker"
+sudo -u ${USER_NAME} newgrp docker
